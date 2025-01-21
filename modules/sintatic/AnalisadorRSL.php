bfdb6ed01e0dfc830d5c665c8ef6a6a80b7ebdbf
Pedro Hoeller <pdanieldeoliveirahoeller@gmail.com>
2025-01-20 21:29:55 -0300

    rodou!!!!RODdOURODOURODOU ASASSSSSSSSSSSSSSSSSSSSSSSSSSS

--- modules/sintatic/AnalisadorRSL.php
+++ modules/sintatic/AnalisadorRSL.php
@@ -4,13 +4,13 @@ class SLRParser {
     private array $gotoTable;
     private array $actionTable;
     private $productions = [
-        [42, 6], [42, 0], [43, 3], [43, 0], [44, 4], [44, 0], [45, 2], [45, 0],
-        [46, 2], [46, 0], [47, 1], [47, 1], [47, 1], [47, 1], [47, 1], [48, 3],
-        [49, 4], [50, 5], [51, 11], [51, 7], [51, 13], [51, 7], [62, 5], [63, 1],
-        [63, 1], [63, 3], [52, 2], [52, 1], [53, 3], [53, 3], [53, 0], [60, 2],
-        [61, 3], [61, 3], [61, 3], [61, 3], [61, 3], [61, 3], [61, 3], [61, 0],
-        [54, 2], [55, 3], [55, 3], [55, 3], [55, 0], [56, 1], [56, 1], [56, 1],
-        [56, 3], [57, 1], [57, 1], [57, 1], [58, 2], [58, 0], [59, 3], [59, 0],
+        ["<Programa>", 6], ["<Programa>", 0], ["<ListaParametros>", 3], ["<ListaParametros>", 0], ["<ListaParametrosRest>", 4], ["<ListaParametrosRest>", 0], ["<ListaComandos>", 2], ["<ListaComandos>", 0],
+        ["<ListaComandosRest>", 2], ["<ListaComandosRest>", 0], ["<Comando>", 1], ["<Comando>", 1], ["<Comando>", 1], ["<Comando>", 1], ["<Comando>", 1], ["<Declaracao>", 3],
+        ["<Atribuicao>", 4], ["<ChamadaFuncao>", 5], ["<ControleFluxo>", 11], ["<ControleFluxo>", 7], ["<ControleFluxo>", 13], ["<ControleFluxo>", 7], ["<Imprime>", 5], ["<ArgumentoImpressao>", 1],
+        ["<ArgumentoImpressao>", 1], ["<ArgumentoImpressao>", 3], ["<Expressao>", 2], ["<Expressao>", 1], ["<ExpressaoRest>", 3], ["<ExpressaoRest>", 3], ["<ExpressaoRest>", 0], ["<ExpressaoLogica>", 2],
+        ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 3], ["<ExpressaoLogicaRest>", 0],
+        ["<Termo>", 2], ["<TermoRest>", 3], ["<TermoRest>", 3], ["<TermoRest>", 3], ["<TermoRest>", 0], ["<Fator>", 1], ["<Fator>", 1], ["<Fator>", 1],
+        ["<Fator>", 3], ["<Tipo>", 1], ["<Tipo>", 1], ["<Tipo>", 1], ["<ListaArgumentos>", 2], ["<ListaArgumentos>", 0], ["<ListaArgumentosRest>", 3], ["<ListaArgumentosRest>", 0],
         [64, 1], [64, 1], [64, 1],
     ];
 
@@ -37,8 +37,6 @@ class SLRParser {
             $token = $tokens[$inputPointer] ?? new Token('$', '$', 0, 0);
             $tokenName = $token->getName();
     
-            echo "Estado atual: $state, Token: $tokenName<br>";
-    
             if (!isset($this->actionTable[$state][$tokenName])) {
                 throw new Exception("Erro de sintaxe na linha " . $token->getLine() . ", token inesperado: " . $tokenName);
             }
@@ -46,27 +44,25 @@ class SLRParser {
             $action = $this->actionTable[$state][$tokenName];
     
             if ($action['type'] === 'SHIFT') {
-                echo "SHIFT para estado: " . $action['state'] . "<br>";
                 $stack[] = $action['state'];
                 $inputPointer++;
             } elseif ($action['type'] === 'REDUCE') {
                 $rule = $this->productions[$action['rule']];
-                echo "REDUCE usando regra: " . $action['rule'] . "<br>";
     
                 for ($i = 0; $i < $rule[1]; $i++) {
                     array_pop($stack);
                 }
+
                 $topState = end($stack);
+
                 $gotoState = $this->gotoTable[$topState][$rule[0]] ?? null;
     
                 if ($gotoState === null) {
                     throw new Exception("Erro ao aplicar redução na linha " . $token->getLine());
                 }
-    
-                echo "GOTO para estado: $gotoState<br>";
+
                 $stack[] = $gotoState;
             } elseif ($action['type'] === 'ACCEPT') {
-                echo "Aceitação bem-sucedida!<br>";
                 return true;
             }
         }
